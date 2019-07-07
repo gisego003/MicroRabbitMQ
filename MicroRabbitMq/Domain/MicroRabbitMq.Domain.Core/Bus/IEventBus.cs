056dbd728e5c4c465480eacb849c3cb0a9c23f87
gsesteaga <gsesteaga@uts.edu.mx>
2019-07-07 01:17:56 -0700

    Implemented interfaces and clases for Domain.Core

---
+++ MicroRabbitMq/Domain/MicroRabbitMq.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbitMq.Domain.Core.Commands;
+using MicroRabbitMq.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbitMq.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<TCommand>(TCommand command) where TCommand : Command;
+
+        void Publish<TEvent>(TEvent @event) where TEvent : Event;
+
+        void Subscribe<TEvent, THandler>()
+            where TEvent : Event
+            where THandler : IEventHandler<TEvent>;
+
+    }
+}
